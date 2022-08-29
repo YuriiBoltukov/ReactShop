f5666064878d14961a53c4efde57d6cada3a709a
YuriiBoltukov <kolomiha898@gmail.com>
2022-08-29 13:52:17 +0300

    feat fetch

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ function Header() {
 		<nav className='deep-purple darken-3'>
 			<div className='nav-wrapper'>
 				<a href='!#' className='brand-logo'>
-					React Movies
+					React Shop Simple
 				</a>
 				<ul id='nav-mobile' className='right hide-on-med-and-down'>
 					<li>
---
+++ src/components/Shop.jsx
@@ -0,0 +1,21 @@
+import React, { useState, useEffect } from 'react';
+import { API_KEY, API_URL } from '../config';
+
+export function Shop() {
+	const [goods, setGoods] = useState([]);
+	const [loading, setLoading] = useState(true);
+
+	useEffect(function getGoods() {
+		fetch(API_URL, {
+			headers: {
+				Authorization: API_KEY,
+			},
+		})
+			.then(response => response.json())
+			.then(data => {
+				data.featured && setGoods(data.featured);
+				setLoading(false);
+			});
+	}, []);
+	return <main className='container content'>shop</main>;
+}
